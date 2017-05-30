01ae6a75777b3a10f7867f9965a874870727bc91
Miguel Julio <mjulio@artnet.com>
2017-05-30 12:02:21 -0400

    clean up package.json and add setup for pact.io

---
+++ api-contract/fixtures/cms-home.js
@@ -0,0 +1,17 @@
+export const sections = [
+    {
+        title: 'Artnet Auctions'
+    },
+    {
+        title: 'Galleries'
+    },
+    {
+        title: 'Auction Houses'
+    },
+    {
+        title: 'Artists'
+    },
+    {
+        title: 'Our Products'
+    }
+]
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,7 @@
 import React from 'react';
-// NOTE: Enzyme uses shallow from 'react-test-renderer'
-// need to install it: npm install --save-dev react-test-renderer
-import {shallow} from 'enzyme';
-
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-
-  shallow(<App />);
-
+  shallow(<App />)
 });
